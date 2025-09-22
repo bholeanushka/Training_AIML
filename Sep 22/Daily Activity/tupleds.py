1842e96e0f0d6d498240e44214825ab928a4d75c
Anushka Bhole <105558175+bholeanushka@users.noreply.github.com>
2025-09-22 12:51:05 +0530

    Add files via upload

---
+++ Sep 22/Daily Activity/tupleds.py
@@ -0,0 +1,10 @@
+colors = ("red", "green", "blue")
+
+print("First Element :",colors[0])    # First Element
+print("Last Element :",colors[-1])   # Last Element
+
+# Unpacking Tuples
+
+c1,c2,c3 = colors
+print(f"\n----Unpacking Tuple---- \nColor 1 : {c1} \nColor 2 : {c2} \nColor 3 : {c3}")
+
