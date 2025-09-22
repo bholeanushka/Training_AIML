d1e41dd8d9e42578bc583bd85a269bc77a0eb282
Anushka Bhole <105558175+bholeanushka@users.noreply.github.com>
2025-09-22 10:54:46 +0530

    Add files via upload

---
+++ Sep 22/Daily Activity/evennodd.py
@@ -0,0 +1,8 @@
+def checknum(num):
+    if num % 2 == 0:
+        return f"{num} is even"
+    else:
+        return f"{num} is odd"
+
+n = int(input("Enter a number: "))
+print(checknum(n))
---
+++ Sep 22/Daily Activity/listds.py
@@ -0,0 +1,10 @@
+l1 = [10,20,30,40]
+# print(l1[0])
+# print(l1[-1])
+
+fruits = ['apple','orange','banana']
+fruits.append('pear')
+fruits.insert(1,'peach')
+print(fruits)
+fruits.remove('pear')
+print(fruits)
